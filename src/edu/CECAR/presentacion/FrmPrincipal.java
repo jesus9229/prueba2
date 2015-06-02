0bce2cf92b68a2e0f5031f8d19d4fe109b4da811
jesus9229 <jesus.rodriguezm@cecar.edu.co>
2015-06-02 15:33:15 -0500

    prueba corregida

--- src/edu/CECAR/presentacion/FrmPrincipal.java
+++ src/edu/CECAR/presentacion/FrmPrincipal.java
@@ -43,7 +43,7 @@ public final class FrmPrincipal extends JFrame {
 	private BufferedReader bfReader;
       
         int x=0;
-        int y=9;
+        int y=91;
 
 /**
  * Creacion del contenido de la interfaz, se implentan las diferentes escuchadoras
